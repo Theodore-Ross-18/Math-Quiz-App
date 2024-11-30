592704c2b4f7540e55a2ccc3a29dbdf56c3e8725
Theodore Ross <115604507+Theodore-Ross-18@users.noreply.github.com>
2024-12-01 05:36:30 +0800

    PHP Session Variables

--- index.php
+++ index.php
@@ -2,7 +2,21 @@
 
 <!-- PHP -->
 <?php
+session_start();
 
+// Initialize session variables
+if (!isset($_SESSION['started'])) {
+    $_SESSION['started'] = false; // Track if the session has started
+}
+if (!isset($_SESSION['correctScore'])) {
+    $_SESSION['correctScore'] = 0; // Store the score for correct answers
+}
+if (!isset($_SESSION['wrongScore'])) {
+    $_SESSION['wrongScore'] = 0; // Store the score for wrong answers
+}
+if (!isset($_SESSION['answered'])) {
+    $_SESSION['answered'] = false; // Track if an answer has been selected
+}
 ?>
 
 <!-- HTML -->
